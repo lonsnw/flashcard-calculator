ccc6013a04e82ffb03cefdece106b4946f60740d
lons <lonsnw@gmail.com>
2024-03-07 10:51:16 -0600

    Updating with HTML tagging

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,21 @@
 console.log('client.js is sourced!');
+
+// establishing as global because i think i might need to use it multiple times
+const equationDiv = document.querySelector('#equation');
+
+
+function addVal(event) {
+    event.preventDefault();
+    console.log('in addVal function');
+    document.getElementById('#numButton')
+    equationDiv.innerHTML += `
+    `
+    
+}
+
+let equation = {
+    numOne: numOne,
+    numTwo: numTwo,
+    operator: operator,
+    result: result
+};
